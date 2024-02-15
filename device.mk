371b6a17c23cefffe7dae68090a338a8470a8306
Woomymy <woomy@woomy.be>
2024-02-15 14:04:06 +0530

    merlinx: Add a shim to link sensor blobs against `android.hardware.sensors@1.0-convert`
    * `libsensorndkbridge` was converted to AIDL and is no longer linked to
      this static library that contains symbols required by our sensor
      blobs. Add a shim linked to `android.hardware.sensors@1.0-convert` to
      fix our sensors
    
    Change-Id: I969f97bb15d0abcc71f87352722995db3826e122
    Signed-off-by: Woomymy <woomy@woomy.be>

--- device.mk
+++ device.mk
@@ -522,6 +522,10 @@ PRODUCT_PACKAGES += \
     libsensorndkbridge \
     libpower.vendor
 
+# Shims
+PRODUCT_PACKAGES += \
+    libpiex_shim
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
