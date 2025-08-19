40c7dba7277cef32ee104c7dbdca6b88630f3ba4
Frost <frost.github@proton.me>
2025-08-19 12:11:01 +0000

    merlinx: Replace libui with libui-v34 for camera libs

--- extract-files.sh
+++ extract-files.sh
@@ -19,6 +19,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libtinyxml.so" "libtinyxml2-v34.so" "${2}"
             ;;
+        vendor/lib64/libcam.hal3a.v3.so | vendor/lib64/libmtkcam_3rdparty.vidhance.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "libui.so" "libui-v34.so" "${2}"
+            ;;
         *)
             return 1
             ;;
