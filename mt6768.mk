36174f4cf56735fe3a2c23ee9886a774722f12e2
me-cafebabe <me.cafebabe@gmail.com>
2026-02-12 00:05:21 +0800

    mt6768-common: overlay: Disable high performance transitions

--- mt6768.mk
+++ mt6768.mk
@@ -347,7 +347,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(COMMON_PATH)/overlay-lineage
+    $(COMMON_PATH)/overlay-lineage \
+    $(COMMON_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
