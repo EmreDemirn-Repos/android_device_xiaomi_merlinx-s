ebd1022b21bfa39b80a35a1ccbd9a74945ba971b
itsaschoolbus <itsaschoolbus@proton.me>
2026-03-24 15:50:24 +0000

    merlinx: Update from V13.0.2.0.SJOMIXM

--- lineage_merlinx.mk
+++ lineage_merlinx.mk
@@ -24,5 +24,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceName=merlinx \
-    BuildDesc="merlin-user 11 RP1A.200720.011 V12.5.4.0.RJOMIXM release-keys" \
-    BuildFingerprint=Redmi/merlin/merlin:11/RP1A.200720.011/V12.5.4.0.RJOMIXM:user/release-keys
+    BuildDesc="merlin-user 12 SP1A.210812.016 V13.0.2.0.SJOMIXM release-keys" \
+    BuildFingerprint=Redmi/merlin/merlin:12/SP1A.210812.016/V13.0.2.0.SJOMIXM:user/release-keys
