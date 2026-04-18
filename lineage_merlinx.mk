c5c1ce22307f4236568312e24ed1a9bfd5a5c568
EmreDemirn <112325107+EmreDemirn@users.noreply.github.com>
2026-04-18 17:54:26 +0300

    merlinx: Update fp to 14.0.0.1.SJOCNXM

--- lineage_merlinx.mk
+++ lineage_merlinx.mk
@@ -23,6 +23,6 @@ PRODUCT_MODEL := Redmi Note 9
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="merlin-user 12 SP1A.210812.016 V13.0.2.0.SJOMIXM release-keys"
+    PRIVATE_BUILD_DESC="merlin-user 12 SP1A.210812.016 V14.0.0.1.SJOCNXM release-keys"
 
-BUILD_FINGERPRINT := Redmi/merlin/merlin:12/SP1A.210812.016/V13.0.2.0.SJOMIXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/merlin/merlin:12/SP1A.210812.016/V14.0.0.1.SJOCNXM:user/release-keys
