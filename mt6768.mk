27156f8b0bffca969efd82df44695b946c1a5cc9
akifakif32 <mhmtkfdrn@gmail.com>
2026-06-25 12:28:36 +0300

    mt6768-common: Add needed modules from source

--- mt6768.mk
+++ mt6768.mk
@@ -142,7 +142,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.memtrack-service.mediatek \
-    android.hardware.memtrack-V1-ndk_platform.vendor
+    android.hardware.memtrack-V1-ndk_platform
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.common@1.2.vendor \
@@ -184,7 +184,7 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.0.vendor \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
     android.hardware.gnss.visibility_control@1.0.vendor \
-    android.hardware.gnss-V1-ndk_platform.vendor
+    android.hardware.gnss-V1-ndk_platform
 
 PRODUCT_PACKAGES += \
     libcurl.vendor
