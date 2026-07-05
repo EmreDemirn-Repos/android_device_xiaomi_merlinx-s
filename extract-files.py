9c4fb90ca664663ccfb618eb8a5447cb79bb68e8
EmreDemirn <emre2021demir@hotmail.com>
2026-07-05 22:41:10 +0000

    merlinx: disable firmware extraction support
    and fix unpinned blob name

--- extract-files.py
+++ extract-files.py
@@ -125,7 +125,7 @@ module = ExtractUtilsModule(
     'xiaomi',
     blob_fixups=blob_fixups,
     namespace_imports=namespace_imports,
-    add_firmware_proprietary_file=True,
+    add_firmware_proprietary_file=False,
 )
 
 if __name__ == '__main__':
