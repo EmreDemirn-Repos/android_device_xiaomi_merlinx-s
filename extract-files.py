d33a40aca90e22e7a7ca689b4c1da14c6bf5d0d4
EmreDemirn <emre2021demir@hotmail.com>
2026-07-09 12:48:54 +0000

    merlinx: add missing comma in extract-files.py

--- extract-files.py
+++ extract-files.py
@@ -48,7 +48,7 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/lib64/libmtkcam_grallocutils.so', 'vendor/lib64/libmtkisp_metadata.so'): blob_fixup()
 	.replace_needed('libui.so', 'libui-v34.so'),
     ('vendor/lib64/libcodec2_mtk_c2store.so', 'vendor/lib64/libcodec2_vpp_qt_plugin.so', 'vendor/lib64/libcodec2_vpp_rs_plugin.so', 'vendor/lib64/libcodec2_mtk_vdec.so', 'vendor/lib64/libcodec2_mtk_venc.so'): blob_fixup()
-        .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so')
+        .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
     'vendor/bin/mnld': blob_fixup()
         .add_needed('libshim_sensors.so'),
     'vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so': blob_fixup()
